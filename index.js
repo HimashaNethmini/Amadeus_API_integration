06feefbdaf2f0445f27a5bac8fb36f37147e5091
Himashaa <himashanethmini05@gmail.com>
2024-11-24 21:01:53 +0530

    make backend to send response to frontend

--- index.js
+++ index.js
@@ -11,28 +11,34 @@ const amadeus = new Amadeus ({
 })
 
 //flight search with price
-const FlightSearch = async () => {
-    try {
-        const flightOffersResponse = await amadeus.shopping.flightOffersSearch.get({
+app.get('/flights', (req,res) => {
+    amadeus.shopping.flightOffersSearch.get({
             originLocationCode: "CMB",
             destinationLocationCode: "MAA",
             departureDate: "2024-11-27",
             adults: "1",
         })
-        console.log("Flight Offers Response:", flightOffersResponse.data);
+        .then (response => {
+            if (response.data && response.data.length >0) {
+                return amadeus.shopping.flightOffers.pricing.post({
+                    'data': {
+                        'type': "flight-offers-pricing",
+                        'flightOffers': [flightOffersResponse.data[0]],
+                    }
+                })
+            } else {
+                throw new Error('No flights offer found')
+            }
+        })
+        .then (pricingResponse => {
+            res.json(pricingResponse.data)
+        })
+        .catch (error => {
+            console.error ('Error showing flight pricing', error.message)
+        })
+})
 
-        const response = await amadeus.shopping.flightOffers.pricing.post(
-            {
-                data: {
-                    type: "flight-offers-pricing",
-                    flightOffers: [flightOffersResponse.data[0]],
-                }
-            },
-        )
-        console.log(response)
-    } catch (error){
-        console.log(error)
-    }
-}
-
-FlightSearch ()
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server on  ${port}`)
+})
