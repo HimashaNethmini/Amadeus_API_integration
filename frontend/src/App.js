69da92e9d12c022e2010e7ee1dd5d00026a69da6
Himashaa <himashanethmini05@gmail.com>
2024-11-24 21:02:15 +0530

    make the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [flights, setFlights ] = useState ([])
+
+  //frontend logic
+useEffect(() => {
+  axios.get('http://localhost:5000/flights')
+  .then(response => {
+    setFlights(response.data)
+  })
+  .catch (error => {
+    console.error("Error", error)
+  })
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {flights.map((flight, index) => (
+        <div key={index} className='flight-card'>
+            <h3> Flights with price</h3>
+            <p>
+              <b>Price : </b> {flight.price.currency} {flight.price.total}
+            </p>
+
+            <p>
+              <b>Seats : </b> {flight.numberOfBookableSeats}
+            </p>
+
+            <p>
+              <b>Flight ID: </b> {flight.id}
+            </p>
+
+            <p>
+              <b>Airline : </b> {flight.validatingAirlineCodes}
+            </p>
+        </div>
+      ))}
     </div>
   );
 }
