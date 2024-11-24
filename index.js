4dd2a9c34a5ced34bd3377518d7822b758e2b6e6
Himashaa <himashanethmini05@gmail.com>
2024-11-24 20:12:20 +0530

    integrate the flight search offer with pricing

---
+++ index.js
@@ -0,0 +1,38 @@
+import Amadeus from 'amadeus'
+import express from 'express'
+import dotenv from 'dotenv';
+const app = express();
+
+dotenv.config();
+
+const amadeus = new Amadeus ({
+    clientId: process.env.Amadeus_ClientId,
+    clientSecret: process.env.Amadeus_ClientSecret
+})
+
+//flight search with price
+const FlightSearch = async () => {
+    try {
+        const flightOffersResponse = await amadeus.shopping.flightOffersSearch.get({
+            originLocationCode: "SYD",
+            destinationLocationCode: "BKK",
+            departureDate: "2024-12-02",
+            adults: "1",
+        })
+        console.log("Flight Offers Response:", flightOffersResponse.data);
+
+        const response = await amadeus.shopping.flightOffers.pricing.post(
+            {
+                data: {
+                    type: "flight-offers-pricing",
+                    flightOffers: [flightOffersResponse.data[0]],
+                }
+            },
+        )
+        console.log(response)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+FlightSearch ()
